b992bf132d1f10c9e494749448e85aafaae81f71
Rafael Gottardi <rafael.gottardi@c6bank.com>
2021-02-09 23:44:09 -0300

    initial commit

--- app/main.py
+++ app/main.py
@@ -1,8 +1,14 @@
 from firebird.driver import connect
 
+print("Python simple Firebird DB  connector")
+print(" Trying to connect...")
 con = connect('/tmp/test.db', user='SYSDBA', password='1234')
 
 cur = con.cursor()
+
+print(" Executing query...")
 cur.execute("select * from table1")
+
+print("Result:")
 print(cur.fetchall())
 
